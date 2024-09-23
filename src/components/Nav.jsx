99d5c7c98d016c2a97dd3ea00b9c9f1179b06391
paprikrumplikas <paprikrumplikas001@gmail.com>
2024-09-23 23:31:26 +0200

    Hero section - 1

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
 
@@ -6,6 +8,9 @@ import { navLinks } from "../constants";
 
 
 const Nav = () => {
+
+    const [toggleDrawer, setToggleDrawer] = useState(false);
+
     return (
         //{/** @learning @crucial this could have been a div but semantic tags are better for readability */ }
         // e.g. <header>, <nav>, <main>, <article>, <section>, <aside>, <footer>, <figure>, <figcaption>, <mark>, <time>
@@ -19,7 +24,7 @@ const Nav = () => {
                         height={29}
                     />
                 </a>
-                <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                <ul className="flex-1 justify-center items-center gap-16 hidden lg:flex">
                     {navLinks.map((item) =>
                         <li key={item.label}>
                             <a
@@ -31,6 +36,35 @@ const Nav = () => {
                         </li>
                     )}
                 </ul>
+
+                {/** hamburger menu for mobile -  hidden for screens larger than sm*/}
+                <div className="lg:hidden">
+                    <img
+                        src={hamburger}
+                        alt="hamburger"
+                        width={25}
+                        height={25}
+                        onClick={() => setToggleDrawer((prev) => (!prev))}
+                    />
+                </div>
+
+                <div
+                    id="drawer-menu"
+                    className={`lg:hidden absolute top-[60px] bg-gray-300 right-0 left-0 z-10 py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
+                    <ul className="gap-16">
+                        {navLinks.map((item) =>
+                            <li key={item.label}>
+                                <a
+                                    href={item.href}
+                                    className="font-montserrat leading-normal text-lg text-slate-grey"
+                                    onClick={() => { setToggleDrawer(false); }}
+                                >
+                                    {item.label}
+                                </a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
 
             </nav>
         </header >
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,82 @@
+import Button from "../components/Button";
+import ShoeCard from "../components/ShoeCard";
+import { arrowRight } from "../assets/icons";
+
+// basic array for values
+import { shoes, statistics } from "../constants";
+
+import { bigShoe1 } from "../assets/images";
+
 
 const Hero = () => {
     return (
-        <div>TEST</div>
-    )
+        // on XL, it is flex-row, on mobile it it flex-col
+        // max-container is our own style
+        <section
+            id="home"
+            className="w-full flex xl:flex-row flex-col justify-center min-h-screen border-2 gap-10 max-container"
+        >
+
+            {/** left part of the hero section: text and stats */}
+            <div
+                className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28"
+            >
+                <p className="text-xl font-montserrat text-coral-red"> Our Summer Collection
+                </p>
+
+                <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+                    {/** on xlarge devices bg is white because on these devices text goes above the image */}
+                    <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+                    <br />
+                    <span className="text-coral-red inline-block mt-3">Nike
+                    </span> Shoes
+                </h1>
+
+                <p className="font-montserrat  text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+
+                <Button
+                    label="Shop now"
+                    iconURL={arrowRight}
+                />
+
+                {/**Container for statistics */}
+                <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+                    {statistics.map((stat) => (
+                        <div
+                            key={stat.label}
+                        >
+                            <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+                            <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+
+            {/** right part of the hero section: img */}
+            <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+                <img
+                    src={bigShoe1}
+                    alt="shoe collection"
+                    width={610}
+                    height={540}
+                    className="object-contain relative z-10 "
+                />
+
+                {/** container for the little 3 images that allows to change the big image */}
+                <div>
+                    {shoes.map((shoe) => (
+                        <div key={shoe}>
+                            <ShoeCard
+                                imgURL={shoe}
+                                changeBigShoeImage={() => { }}
+                                bigShoeImgage=""
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>)
 }
 
 export default Hero
